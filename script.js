cbd2f5bbae6bb5a6eba5663f2df3a05981f53948
alizul01 <alizulfikar032@gmail.com>
2022-03-04 11:32:57 +0700

    Membuat To Do List Website

---
+++ script.js
@@ -0,0 +1,48 @@
+// Selector
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const completeButton = document.querySelectorAll(".complete-btn");
+const deleteButton = document.querySelectorAll(".delete-btn");
+
+// Event Listener
+todoButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    // Todo
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // Create LI
+    const newListTodo = document.createElement("li");
+    newListTodo.innerText = todoInput.value;
+    newListTodo.classList.add("todo-item");
+    todoDiv.appendChild(newListTodo);
+
+    // Button Completed and Delete
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
+
+    todoList.appendChild(todoDiv);
+
+    todoInput.value = "";
+});
+
+todoList.addEventListener("click", deleteList);
+
+// Function
+
+function deleteList(e) {
+    const item = e.target;
+
+    if (item.classList[0] === "delete-btn") {
+        const todo = item.parentElement;
+        todo.remove();
+    }
+}
